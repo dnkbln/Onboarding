20709aa625fed0c1a0b38699ec27d855308f0f56
Dirk Schumann <dirk@freunde-aus-berlin.net>
2021-01-16 18:57:15 +0100

    Start Onboarding-App

---
+++ src/Onboarding.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <div class="wrapper-mains">
+      <div class="left-align">
+        <onboarding-steps :numStep="5" :actStep="3"></onboarding-steps>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import OnboardingSteps from './components/OnboardingSteps.vue'
+export default {
+  components:{
+    OnboardingSteps
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
